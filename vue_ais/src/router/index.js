c4776352b2294d6851ab2a1ec784c5b0e00c4bf6
JV <jakub.vlcak888@gmail.com>
2024-12-06 21:13:04 +0100

    data zo store.js -> data.json

--- vue_ais/src/router/index.js
+++ vue_ais/src/router/index.js
@@ -4,6 +4,7 @@ import ScheduleView from '../views/ScheduleView.vue'
 import HomeView from '../views/HomeView.vue'
 import SubjectsView from '@/views/SubjectsView.vue'
 
+
 const routes = [
   { path: '/',  name: 'home', component: HomeView },
   { path: '/schedule', name: 'schedule', component: ScheduleView },
